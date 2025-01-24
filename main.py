9b7ebde9cc900c5edf3ddefffa3df02938d4f41e
thatdarnedbob <123yiaFTUTi121yiuhjk>
2025-01-23 19:14:16 -0600

    Changed control to groups

--- main.py
+++ main.py
@@ -14,6 +14,12 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
 
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while(True):
@@ -22,11 +28,14 @@ def main():
                 return
         screen.fill("black")
 
-        player.draw(screen)
+        for thing in updatable:
+            thing.update(dt)
+        
+        for thing in drawable:
+            thing.draw(screen)
         
         pygame.display.flip()
         dt = clock.tick(60)/1000
-        player.update(dt)
 
 if __name__ == "__main__":
     main()
