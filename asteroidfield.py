15c5f2eb51bbe0fbe13f66ba749c168cb72ec690
thatdarnedbob <123yiaFTUTi121yiuhjk>
2025-01-23 19:36:50 -0600

    Fixed asteroid spawn bug

--- asteroidfield.py
+++ asteroidfield.py
@@ -41,13 +41,13 @@ class AsteroidField(pygame.sprite.Sprite):
             self.spawn_timer = 0
 
         # spawn a new asteroid at a random edge
-        edge = random.choice(self.edges)
-        speed = random.randint(40, 100)
+            edge = random.choice(self.edges)
+            speed = random.randint(40, 100)
 
-        velocity = edge[0] * speed
-        velocity = velocity.rotate(random.randint(-30,30))
+            velocity = edge[0] * speed
+            velocity = velocity.rotate(random.randint(-30,30))
 
-        position = edge[1](random.uniform(0, 1))
-        kind = random.randint(1, ASTEROID_KINDS)
+            position = edge[1](random.uniform(0, 1))
+            kind = random.randint(1, ASTEROID_KINDS)
 
-        self.spawn(ASTEROID_MIN_RADIUS * kind, position, velocity)
+            self.spawn(ASTEROID_MIN_RADIUS * kind, position, velocity)
