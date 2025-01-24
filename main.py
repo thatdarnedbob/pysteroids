cca2439081886ff06de66305d0fab5b936d1b83e
thatdarnedbob <123yiaFTUTi121yiuhjk>
2025-01-23 19:05:47 -0600

    Added movement + turning

--- main.py
+++ main.py
@@ -20,10 +20,13 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill((0, 0, 0))
-        dt = clock.tick(60)/1000
+        screen.fill("black")
 
         player.draw(screen)
+        
+        pygame.display.flip()
+        dt = clock.tick(60)/1000
+        player.update(dt)
 
 if __name__ == "__main__":
     main()
