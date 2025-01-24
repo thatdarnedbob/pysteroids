62b1a48b30b3647ef0048642cd10ff2ece876453
thatdarnedbob <123yiaFTUTi121yiuhjk>
2025-01-23 19:32:38 -0600

    Added asteroid field

--- circleshape.py
+++ circleshape.py
@@ -13,6 +13,7 @@ class CircleShape(pygame.sprite.Sprite):
         self.position = pygame.Vector2(x, y)
         self.velocity = pygame.Vector2(0, 0)
         self.radius = radius
+        
     def draw(self, screen):
         # sub-classes must override
         pass
--- main.py
+++ main.py
@@ -2,6 +2,8 @@
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     # print("Starting asteroids!")
@@ -17,10 +19,14 @@ def main():
 
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
 
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
 
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroid_field = AsteroidField()
 
     while(True):
         for event in pygame.event.get():
